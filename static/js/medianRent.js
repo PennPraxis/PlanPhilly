60fae0669d15bec44929819293c4b6b8125294b4
hamhands <patrick.t.hammons@gmail.com>
2014-10-01 20:04:59 -0400

    add basic map

---
+++ static/js/medianRent.js
@@ -0,0 +1,16 @@
+var map = L.map('basic-map').setView([39.952299, -75.163256], 11);
+
+L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20
+}).addTo(map);
+
+
+var rentURL = "/static/data/medianRent.geojson";
+
+$.getJSON(rentURL, function(data) {
+    L.geoJson(data, {
+    }).addTo(map)
+});
