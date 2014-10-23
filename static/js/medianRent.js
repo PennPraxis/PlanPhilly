505989cbf9dcc7b3ca7e5ff3b08bd3d4975ea222
hamhands <patrick.t.hammons@gmail.com>
2014-10-22 20:10:06 -0400

    test

--- static/js/medianRent.js
+++ static/js/medianRent.js
@@ -1,42 +1,72 @@
+//test comment
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGFtaGFuZHMiLCJhIjoiMksybk92QSJ9.TOMmDM4uWCY65kSLpS_Nww';
 var map = L.mapbox.map('map').setView([39.952299, -75.163256], 11);
 var layers = document.getElementById('menuUI');
 
-addLayer(L.mapbox.featureLayer()
+function getColor(d) {
+    return d > 1600 ? '#00441b' :
+            d > 1400 ? '#006d2c' :
+           d > 1200  ? '#238b45' :
+           d > 1000  ? '#41ab5d' :
+           d > 800  ? '#74c476' :
+           d > 600  ? '#a1d99b' :
+           d > 400   ? '#c7e9c0' :
+           d > 200   ? '#e5f5e0' :
+                      '#f7fcf5';
+};
+
+function style90(feature) {
+    return {
+        fillColor: getColor(feature.properties.MRENT90_A),
+        weight: 1,
+        color: 'white',
+        opacity: 1,
+        fillOpacity: 0.85
+    };
+};
+
+function style00(feature) {
+    return {
+        fillColor: getColor(feature.properties.MRENT00_A),
+        weight: 1,
+        color: 'white',
+        opacity: 1,
+        fillOpacity: 0.85
+    };
+};
+
+function style12(feature) {
+    return {
+        fillColor: getColor(feature.properties.MRENT12_A),
+        weight: 1,
+        color: 'white',
+        opacity: 1,
+        fillOpacity: 0.85
+    };
+};
+
+var nineteenNinety = L.mapbox.featureLayer()
     .loadURL('static/data/medianRent.geojson')
-    .on('ready', addLayer), '1990', 1);
-addLayer(L.mapbox.featureLayer()
+    .on('ready', addLayer);
+
+twoThousand = L.mapbox.featureLayer()
     .loadURL('static/data/medianRent.geojson')
-    .on('ready', addLayer), '2000', 2);
-addLayer(L.mapbox.featureLayer()
+    .on('ready', addLayer);
+
+twoThousandTwelve = L.mapbox.featureLayer()
     .loadURL('static/data/medianRent.geojson')
-    .on('ready', addLayer), '2010', 3);
+    .on('ready', addLayer);
+
+
+addLayer(nineteenNinety, '1990', 1);
+addLayer(twoThousand, '2000', 2);
+addLayer(twoThousandTwelve, '2012', 3);
 
 //add tiles
 var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
 }).addTo(map);
-//add tracts
-/*
-var tractsURL = "/static/data/medianRent.geojson";
 
-$.getJson(tractsURL, function(data) {
-    var ninety = L.geoJson(data, {
-        style: function(feature) {
-            return {
-                fillColor: getColor(feature.properties.MRENT90_A),
-                weight: 1,
-                color: 'white',
-                opacity: 1,
-                fillOpacity: 0.85
-            };
-        }
-    });
-});
-*/
-//add layers to ui
-
-//add control
 function addLayer(layer, name, zIndex) {
     layer
         .setZIndex(zIndex)
@@ -51,12 +81,12 @@ function addLayer(layer, name, zIndex) {
         e.preventDefault();
         e.stopPropagation();
 
-        if (this.className == 'active' && map.hasLayer(layer)) {
+        if (map.hasLayer(layer)) {
             map.removeLayer(layer);
-            this.className = 'active';
+            this.className = '';
         } else {
             map.addLayer(layer);
-            this.className = '';
+            this.className = 'active';
         }
     };
 
