1a80cd9fdc7d212a2654711b475767d87bcea2cc
hamhands <patrick.t.hammons@gmail.com>
2014-10-23 15:59:51 -0400

    fix UI

--- static/js/medianRent.js
+++ static/js/medianRent.js
@@ -1,17 +1,17 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGFtaGFuZHMiLCJhIjoiMksybk92QSJ9.TOMmDM4uWCY65kSLpS_Nww';
 var map = L.mapbox.map('map').setView([39.952299, -75.163256], 11);
-var layers = document.getElementById('menuUI');
-
+//var layers = document.getElementById('menuUI');
+/*
 addLayer(L.mapbox.featureLayer()
     .loadURL('static/data/medianRent.geojson')
-    .on('ready', addLayer), '1990', 1);
+    , '1990', 1);
 addLayer(L.mapbox.featureLayer()
     .loadURL('static/data/medianRent.geojson')
     , '2000', 2);
 addLayer(L.mapbox.featureLayer()
     .loadURL('static/data/medianRent.geojson')
     , '2010', 3);
-
+*/
 //add tiles
 var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
@@ -37,6 +37,7 @@ $.getJson(tractsURL, function(data) {
 //add layers to ui
 
 //add control
+/*
 function addLayer(layer, name, zIndex) {
     layer
         .setZIndex(zIndex)
@@ -51,15 +52,25 @@ function addLayer(layer, name, zIndex) {
         e.preventDefault();
         e.stopPropagation();
 
-        if (this.className == 'active' && map.hasLayer(layer)) {
-            map.addLayer(layer);
+        if (map.hasLayer(layer)) {
+            map.removeLayer(layer);
             this.className = 'active';
         } else {
-            map.removeLayer(layer);
+            map.addLayer(layer);
             this.className = '';
         }
     };
 
     layers.appendChild(link);
 }
+*/
 
+L.control.layers({
+      '1990': L.mapbox.featureLayer()
+        .loadURL('static/data/medianRent.geojson').addTo(map),
+      '2000': L.mapbox.featureLayer()
+        .loadURL('static/data/medianRent.geojson'),
+      '2012': L.mapbox.featureLayer()
+        .loadURL('static/data/medianRent.geojson')
+  },{}
+  ).addTo(map);
